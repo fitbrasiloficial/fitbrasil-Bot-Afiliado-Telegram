affb8e8d5d6f00853ad34888750b6cb756ef4256
murilo813 <murilodesouza555@gmail.com>
2025-04-22 13:29:50 -0300

    subindo projeto pro github

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from urllib.parse import urlparse
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstallerfrom 
+import chromedriver_autoinstaller
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
